d3555b396902e306bde3505ad081f98d1d532af3
Li Chao <leetschau@gmail.com>
2019-12-24 18:55:46 +0800

    add Sequence and While in small-step semantics

--- chapter2/SmallStepEnv.rb
+++ chapter2/SmallStepEnv.rb
@@ -33,9 +33,9 @@ end
 class Multiply
   def reduce(environment)
     if left.reducible?
-      Add.new(left.reduce(environment), right)
+      Multiply.new(left.reduce(environment), right)
     elsif right.reducible?
-      Add.new(left, right.reduce(environment))
+      Multiply.new(left, right.reduce(environment))
     else
       Number.new(left.value * right.value)
     end
@@ -43,11 +43,11 @@ class Multiply
 end
 
 class LessThan
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      LessThan.new(left.reduce, right)
+      LessThan.new(left.reduce(environment), right)
     elsif right.reducible?
-      LessThan.new(left, right.reduce)
+      LessThan.new(left, right.reduce(environment))
     else
       Boolean.new(left.value < right.value)
     end
@@ -74,6 +74,7 @@ end
 Machine.new(
   Add.new(Variable.new(:x), Variable.new(:y)),
   {x: Number.new(3), y: Number.new(4) }).run
+puts "-----------"
 
 class DoNothing
   def to_s
@@ -119,6 +120,7 @@ statement = Assign.new(:x, Add.new(Variable.new(:x), Number.new(13)))
 puts statement
 puts statement.reducible?
 environment = { x: Number.new(22) }
+puts "-----------"
 
 # 手工循环对语句求值
 statement, environment = statement.reduce(environment)
@@ -130,4 +132,131 @@ puts statement.reducible?
 statement, environment = statement.reduce(environment)
 puts statement, environment
 puts statement.reducible?
+puts "-----------"
+
+Object.send(:remove_const, :Machine)
+
+class Machine < Struct.new(:statement, :environment)
+  def step
+    self.statement, self.environment = statement.reduce(environment)
+  end
+
+  def run
+    while statement.reducible?
+      puts "#{statement}, #{environment}"
+      step
+    end
+
+    puts "#{statement}, #{environment}"
+  end
+end
+
+Machine.new(
+  Assign.new(:x, Add.new(Variable.new(:x), Number.new(1))),
+  { x: Number.new(2) }).run
+puts "-----------"
+
+class If < Struct.new(:condition, :consequence, :alternative)
+  def to_s
+    "if (#{condition}) { #{consequence} } else { #{alternative} }"
+  end
+
+  def inspect
+    "《#{self}》"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    if condition.reducible?
+      [If.new(condition.reduce(environment), consequence, alternative), environment]
+    else
+      case condition
+      when Boolean.new(true)
+        [consequence, environment]
+      when Boolean.new(false)
+        [alternative, environment]
+      end
+    end
+  end
+end
+
+Machine.new(
+  If.new(
+    Variable.new(:x),
+    Assign.new(:y, Number.new(1)),
+    Assign.new(:y, Number.new(2))
+  ),
+  { x: Boolean.new(true) }
+).run
+puts "-----------"
+
+Machine.new(
+  If.new(Variable.new(:x), Assign.new(:y, Number.new(1)), DoNothing.new),
+  { x: Boolean.new(false) }
+).run
+puts "-----------"
+
+class Sequence < Struct.new(:first, :second)
+  def to_s
+    "#{first}; #{second}"
+  end
+
+  def inspect
+    "《#{self}》"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    case first
+    when DoNothing.new
+      [second, environment]
+    else
+      reduced_first, reduced_environment = first.reduce(environment)
+      [Sequence.new(reduced_first, second), reduced_environment]
+    end
+  end
+end
+
+Machine.new(
+  Sequence.new(
+    Assign.new(:x, Add.new(Number.new(1), Number.new(1))),
+    Assign.new(:y, Add.new(Variable.new(:x), Number.new(3)))
+  ),
+  {}
+).run
+puts "-----------"
+
+class While < Struct.new(:condition, :body)
+  def to_s
+    "while (#{condition}) { #{body} }"
+  end
+
+  def inspect
+    "《#{self}》"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    [If.new(condition, Sequence.new(body, self), DoNothing.new), environment]
+    # 把整个 while 循环放在 body 后面，形成新的语句序列
+  end
+end
+
+Machine.new(
+  While.new(
+    LessThan.new(Variable.new(:x), Number.new(5)),
+    Assign.new(:x, Multiply.new(Variable.new(:x), Number.new(3)))
+  ),
+  { x: Number.new(1) }
+).run
+puts "-----------"
 
